62129ef85e0bcfba8ea12278e021885f3d49901c
Silence <374619540@qq.com>
2021-06-22 22:28:39 +0800

    HelloQt

---
+++ 02_HelloFFmpeg/main.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+// 在QT中，不能使用C++标准的cout来打印信息
+//#include <iostream>
+
+#include <QDebug>
+
+// FFmpeg是纯C语言的
+// C++是不能直接导入C语言函数的
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    qDebug() << av_version_info();
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
