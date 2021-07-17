bf70f247e389f7a5ecf07c565c3b2049c40c6289
Silence <374619540@qq.com>
2021-07-17 20:30:46 +0800

    update

---
+++ FFmpeg-xcode/FFmpeg-xcode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FFmpeg-xcode
+//
+//  Created by Silence on 2021/7/17.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
